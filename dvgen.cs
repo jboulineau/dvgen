54d26ec891ad92384312237b9510d27be8844931
jboulineau@gmail.com <BIL8068@HCA.corpad.net>
2018-06-08 12:51:51 -0500

    Basic setup

---
+++ dvgen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CommandLineParser.Core;
+
+namespace dvgen
+{
+    public class CommandLineArguments 
+    {
+        public int test {get; set;}
+    }
+
+    public class Program
+    {
+        private static FluentCommandLineParser<CommandLineArguments> parser = new FluentCommandLineParser<CommandLineArguments>();
+        
+        static void Main(string[] args)
+        {
+            SetupArgs();
+
+            var result = parser.Parse(args);
+
+            if(!result.HelpCalled)
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            Console.WriteLine(result.ErrorText);
+        }
+
+        private static void SetupArgs()
+        {
+            parser.SetupHelp("?", "help")
+            .Callback(text => Console.WriteLine(text))
+            .UseForEmptyArgs();
+
+            parser.Setup(a => a.test)
+                .As('t',"test")
+                .WithDescription("This is a test")
+                .Required();
+        }
+    }
+}
